9d77d1155e4886ff86fd3179529d14b6ad37cfd2
Denis Julio <djms1234@gmail.com>
2022-06-20 07:42:02 -0300

    implements keyboard functionality for adding items to the list

--- src/components/list-ui/InputListContent.tsx
+++ src/components/list-ui/InputListContent.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FocusEvent, useRef, useState } from "react";
 import styles from "./ListUi.module.sass";
 
 export default function InputListContent({
@@ -7,8 +7,20 @@ export default function InputListContent({
   onNewListItemAdded: (content: string) => void;
 }) {
   const [content, setContent] = useState("");
+  const inputRef = useRef<HTMLInputElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  addEventListener("keydown", (e) => {
+    if (e.code === "Slash") {
+      if (inputRef.current) {
+        e.preventDefault();
+        inputRef.current.focus();
+      }
+    }
+  });
 
   const addNewListItem = () => {
+    console.log(`content: ${content}`);
     if (content) {
       onNewListItemAdded(content);
     }
@@ -17,17 +29,33 @@ export default function InputListContent({
 
   const onChangeHandler = (evt: ChangeEvent<HTMLInputElement>) => {
     setContent(evt.target.value);
+    console.log(`content on change ${content}`);
+  };
+
+  const onFocusHandler = (evt: FocusEvent<HTMLInputElement>) => {
+    const input = evt.target as HTMLInputElement;
+    input.addEventListener("keydown", (keyEvt) => {
+      if (keyEvt.code === "Enter")
+        if (buttonRef.current) buttonRef.current.click();
+      if (keyEvt.code === "Escape") input.blur();
+    });
   };
 
   return (
     <div>
       <input
-        className={styles["inputList-input"]}
         value={content}
+        className={styles["inputList-input"]}
+        ref={inputRef}
         type="text"
         onChange={onChangeHandler}
+        onFocus={onFocusHandler}
       />
-      <button className={styles.button} onClick={addNewListItem}>
+      <button
+        ref={buttonRef}
+        className={styles.button}
+        onClick={addNewListItem}
+      >
         add
       </button>
     </div>
